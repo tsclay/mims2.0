3d3232f811694a045bd78b57ad95ac88e6da0a1c
Tim Clay <tsclay9@gmail.com>
2020-09-14 19:45:57 -0400

    init & work on header component

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Header from './components/Header.svelte'
+  import { windowWidth } from './stores.js'
+  export let name
+  let needModal = false
+  let width
+  const unsubscribe = windowWidth.subscribe((value) => (width = value))
+</script>
+
+<style>
+</style>
+
+<svelte:window
+  on:resize={() => {
+    windowWidth.set(window.innerWidth)
+  }} />
+
+<svelte:body on:click={() => (needModal = false)} />
+
+<div>
+  <p>The size is {width}</p>
+  <Header {needModal} {width} />
+  <p>Hello {name}</p>
+</div>
---
+++ src/components/Header.svelte
@@ -0,0 +1,51 @@
+<script>
+  export let needModal
+  export let width
+
+  console.log('this is the modal toggle', needModal)
+
+  const toggleNavButtons = () => {
+    console.log('inside the toggle function!')
+    needModal = !needModal
+  }
+</script>
+
+<style>
+  #header {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .sm-modal {
+    position: absolute;
+    top: 2rem;
+    right: 2rem;
+    background: gray;
+    border-radius: 4px;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: start;
+  }
+</style>
+
+<div id="header">
+  <h1>Mims Family Painting</h1>
+  <div class="button-group">
+    {#if width > 600}
+      <button type="button">About</button>
+      <button type="button">Gallery</button>
+      <button type="button">Contact</button>
+    {:else}
+      <button on:click={toggleNavButtons}>Menu</button>
+      {#if needModal === true}
+        <div class="sm-modal">
+          <button type="button">About</button>
+          <button type="button">Gallery</button>
+          <button type="button">Contact</button>
+        </div>
+      {/if}
+    {/if}
+  </div>
+</div>
