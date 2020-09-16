cb6c66efeeb43217062678d3d73e6589098399fe
Tim Clay <tsclay9@gmail.com>
2020-09-16 00:18:17 -0400

    create About component ; not added to App yet

---
+++ src/components/About.svelte
@@ -0,0 +1,54 @@
+<script>
+</script>
+
+<style>
+</style>
+
+<div id="about-section">
+  <p>
+    Serving Nassau and Suffolk Counties on Long Island, Westchester County, and
+    New York City
+  </p>
+  <p>
+    Proud to be one of the largest long-established high-quality painting
+    companies in New York.
+  </p>
+  <p>A leading fully insured, licensed and bonded painting contractor</p>
+  <p>
+    Six generations of professionalism, experience, highly-skilled workmanship,
+    outstanding customer relations and exceptional service
+  </p>
+  <p>
+    More than 75 well-trained, experienced painters will be at your disposal, 24
+    hours a day, 365 days a year. Same day emergency service is available, as
+    needed.
+  </p>
+  <p>
+    Our company works closely with builders, architects, designers, and
+    homeowners to ensure they will get a well run, quality paint job.
+  </p>
+  <p>
+    Our residential repaint work caters to a wide spectrum of homes, from
+    mansions to cottages. All projects undertaken are family run with utmost
+    efficiency, and care is taken to accommodate any given environment.
+  </p>
+  <p>
+    Our commercial work experience includes banks, office buildings,
+    restaurants, nursing homes, lighthouses and retail stores.
+  </p>
+  <!-- <p>
+    Depend on Mims Family Painting to give the same care and attention to every
+    job, from the smallest project (one door is not too small) to the largest
+    project. We are meticulous and demand the best from our employees.
+  </p> -->
+  <!-- This should go close to contact form -->
+  <p>
+    Our pricing is competitive with other quality contractors but we will work
+    with you on budget limitations as needed. We offer a variety of payment
+    options to accommodate your budget, as well as zero interest payment plans.
+  </p>
+  <p>
+    Estimates are provided quickly and at no cost. Painting one door, painting
+    an entire house; you name it, we'll quote it.
+  </p>
+</div>
