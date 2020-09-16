15810da1a9ade6dc0811ef4961298ed0a2614ec0
Tim Clay <tsclay9@gmail.com>
2020-09-16 00:17:59 -0400

    insert Footer component and pass it props to toggle sticky nav

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,16 @@
   import NavBar from './components/NavBar.svelte'
   import ContactForm from './components/ContactForm.svelte'
   import Banner from './components/Banner.svelte'
+  import Footer from './components/Footer.svelte'
   import { windowWidth, needModal } from './stores.js'
 
   export let toggleModal
   export let width
+  let navIsSticky = false
+  const stickyNav = `position: sticky;
+    position: -webkit-sticky;
+    top: 0;
+    left: 0;`
 
   const unsubscribeWidth = windowWidth.subscribe((value) => (width = value))
   const unsubscribeModal = needModal.subscribe((value) => {
@@ -14,6 +20,10 @@
 
   const toggleNavButtons = () => {
     needModal.set(!toggleModal)
+  }
+
+  const toggleStickyNav = () => {
+    window.scrollY >= 700 ? (navIsSticky = true) : (navIsSticky = false)
   }
 </script>
 
@@ -29,12 +39,14 @@
   }} />
 
 <svelte:window
+  on:scroll={toggleStickyNav}
   on:resize={() => {
     windowWidth.set(window.innerWidth)
   }} />
 
 <div>
   <Banner />
-  <NavBar {width} {toggleModal} {toggleNavButtons} />
+  <NavBar {width} {toggleModal} {toggleNavButtons} {navIsSticky} {stickyNav} />
   <ContactForm {width} />
+  <Footer />
 </div>
