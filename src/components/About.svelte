af6df1b95c2d947a4208abdef38cf7d0ea380286
Tim <tsclay9@gmail.com>
2020-09-16 18:09:33 -0400

    implemented img/text relationship in About with styling

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,54 +1,111 @@
 <script>
 </script>
 
-<style>
+<style type="text/scss">
+  #about-section > div {
+    margin-bottom: 4rem;
+  }
+  .about-containers {
+    display: flex;
+    align-items: flex-start;
+    flex-flow: row nowrap;
+    img {
+      max-width: 398px;
+      height: 298px;
+    }
+  }
+
+  .pic-left-side {
+    img {
+      margin-right: 1rem;
+    }
+  }
+
+  .pic-right-side {
+    img {
+      margin-left: 1rem;
+    }
+  }
+
+  .about-text {
+    h2 {
+      margin-top: 0;
+    }
+  }
 </style>
 
 <div id="about-section">
-  <p>
-    Serving Nassau and Suffolk Counties on Long Island, Westchester County, and
-    New York City
-  </p>
-  <p>
-    Proud to be one of the largest long-established high-quality painting
-    companies in New York.
-  </p>
-  <p>A leading fully insured, licensed and bonded painting contractor</p>
-  <p>
-    Six generations of professionalism, experience, highly-skilled workmanship,
-    outstanding customer relations and exceptional service
-  </p>
-  <p>
-    More than 75 well-trained, experienced painters will be at your disposal, 24
-    hours a day, 365 days a year. Same day emergency service is available, as
-    needed.
-  </p>
-  <p>
-    Our company works closely with builders, architects, designers, and
-    homeowners to ensure they will get a well run, quality paint job.
-  </p>
-  <p>
-    Our residential repaint work caters to a wide spectrum of homes, from
-    mansions to cottages. All projects undertaken are family run with utmost
-    efficiency, and care is taken to accommodate any given environment.
-  </p>
-  <p>
-    Our commercial work experience includes banks, office buildings,
-    restaurants, nursing homes, lighthouses and retail stores.
-  </p>
+  <h1>About</h1>
+  <div class="about-containers pic-left-side">
+    <img src="./assets/history.jpg" alt="Mims Painting, 1925" />
+    <div class="about-text">
+      <h2>Made in New York, Made for New York</h2>
+      <p>
+        We are built on six generations of highly-skilled workmanship,
+        outstanding customer relations, and exceptional service in the painting
+        industry.
+      </p>
+      <p>
+        We serve Nassau and Suffolk Counties on Long Island, Westchester County,
+        and New York City, and we're proud to be one of the largest
+        long-established high-quality painting companies in New York.
+      </p>
+    </div>
+  </div>
+
+  <div class="about-containers pic-right-side">
+    <div class="about-text">
+      <h2>Fully licensed, insured, bonded, and certified</h2>
+      <p>
+        More than 75 well-trained, experienced painters will be at your
+        disposal, 24 hours a day, 365 days a year. Same day emergency service is
+        available, as needed.
+      </p>
+      <p>
+        Our company works closely with builders, architects, designers, and
+        homeowners to ensure they will get a well run, quality paint job.
+      </p>
+    </div>
+    <img
+      src="./assets/artsy.jpg"
+      alt="Paint work on non-conventional canvases" />
+  </div>
+
+  <div class="about-containers pic-left-side">
+    <img src="./assets/houseWork.jpg" alt="Painting a house in the Hamptons" />
+    <div class="about-text">
+      <h2>Serving Residential & Commercial</h2>
+      <p>
+        Our residential repaint work caters to a wide spectrum of homes, from
+        mansions to cottages. All projects undertaken are family run with utmost
+        efficiency, and care is taken to accommodate any given environment.
+      </p>
+      <p>
+        Our commercial work experience includes banks, office buildings,
+        restaurants, nursing homes, lighthouses and retail stores.
+      </p>
+    </div>
+  </div>
+
   <!-- <p>
     Depend on Mims Family Painting to give the same care and attention to every
     job, from the smallest project (one door is not too small) to the largest
     project. We are meticulous and demand the best from our employees.
   </p> -->
   <!-- This should go close to contact form -->
-  <p>
-    Our pricing is competitive with other quality contractors but we will work
-    with you on budget limitations as needed. We offer a variety of payment
-    options to accommodate your budget, as well as zero interest payment plans.
-  </p>
-  <p>
-    Estimates are provided quickly and at no cost. Painting one door, painting
-    an entire house; you name it, we'll quote it.
-  </p>
+
+  <div class="about-containers pic-right-side">
+    <div class="about-text">
+      <h2>Competitve, flexible pricing</h2>
+      <p>
+        Our pricing is competitive with other quality contractors but we will
+        work with you on budget limitations as needed. We offer a variety of
+        payment options to accommodate your budget, as well as zero interest
+        payment plans.
+      </p>
+    </div>
+    <img
+      src="./assets/window.png"
+      alt="Painter on a ladder painting a window" />
+  </div>
 </div>
