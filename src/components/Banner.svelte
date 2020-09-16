5c1e9386701959e93bcc3cf48712acc85c8e4eba
Tim Clay <tsclay9@gmail.com>
2020-09-15 23:13:51 -0400

    made banner component

---
+++ src/components/Banner.svelte
@@ -0,0 +1,38 @@
+<script>
+</script>
+
+<style>
+  img {
+    width: 100%;
+    height: 700px;
+    position: relative;
+  }
+
+  .banner-text-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .flex-text {
+    position: absolute;
+    display: flex;
+    height: 100%;
+    width: 100%;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
+
+<div id="banner">
+  <img src="../assets/banner-img.jpg" alt="Mims Family Painting" />
+  <div class="banner-text-wrapper">
+    <div class="flex-text">
+      <h1>Mims Family Painting</h1>
+      <h3>Six generations of custom painting</h3>
+    </div>
+  </div>
+</div>
