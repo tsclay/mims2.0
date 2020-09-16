737045cde29f02efb0cf94716e8242f52c357dfe
Tim Clay <tsclay9@gmail.com>
2020-09-16 00:18:29 -0400

    made Footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<style>
+  #footer {
+    background: rgb(255, 246, 126);
+    height: 10rem;
+  }
+</style>
+
+<div id="footer">
+  <p>Copyright © 1868-2020 Mims Family Painting All Rights Reserved.</p>
+  <p>Website designed by Tim Clay.</p>
+</div>
