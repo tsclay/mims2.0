d9c913aab240999dbc5e1d6e6e5e29a5f071342f
Tim <tsclay9@gmail.com>
2020-09-16 18:10:49 -0400

    beginning testimonials component

---
+++ src/components/Testimonials.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<style>
+  #testimonials {
+  }
+</style>
+
+<div id="testimonials">
+  <p>
+    "Thank you for the superb craft you bring to our colors. -Don, Donald
+    Kaufman Color"
+  </p>
+  <p />
+</div>
