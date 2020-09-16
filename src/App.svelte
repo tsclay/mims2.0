3ed9c0e8f6e53b148675420f9cf224d471b7ac9e
Tim <tsclay9@gmail.com>
2020-09-16 18:08:45 -0400

    added About and wrapped main content in main tag to center on page

--- src/App.svelte
+++ src/App.svelte
@@ -1,52 +1,61 @@
 <script>
-  import NavBar from './components/NavBar.svelte'
-  import ContactForm from './components/ContactForm.svelte'
-  import Banner from './components/Banner.svelte'
-  import Footer from './components/Footer.svelte'
-  import { windowWidth, needModal } from './stores.js'
+  import NavBar from "./components/NavBar.svelte";
+  import ContactForm from "./components/ContactForm.svelte";
+  import Banner from "./components/Banner.svelte";
+  import About from "./components/About.svelte";
+  import Footer from "./components/Footer.svelte";
+  import { windowWidth, needModal } from "./stores.js";
 
-  export let toggleModal
-  export let width
-  let navIsSticky = false
+  export let toggleModal;
+  export let width;
+  let navIsSticky = false;
   const stickyNav = `position: sticky;
     position: -webkit-sticky;
     top: 0;
-    left: 0;`
+    left: 0;`;
 
-  const unsubscribeWidth = windowWidth.subscribe((value) => (width = value))
+  const unsubscribeWidth = windowWidth.subscribe((value) => (width = value));
   const unsubscribeModal = needModal.subscribe((value) => {
-    toggleModal = value
-  })
+    toggleModal = value;
+  });
 
   const toggleNavButtons = () => {
-    needModal.set(!toggleModal)
-  }
+    needModal.set(!toggleModal);
+  };
 
   const toggleStickyNav = () => {
-    window.scrollY >= 700 ? (navIsSticky = true) : (navIsSticky = false)
-  }
+    window.scrollY >= 700 ? (navIsSticky = true) : (navIsSticky = false);
+  };
 </script>
 
-<style>
+<style type="text/scss">
+  main {
+    margin: 0 auto;
+    width: 90%;
+    max-width: 1298px;
+  }
 </style>
 
 <svelte:body
   on:click={(e) => {
-    console.log(e.target)
+    console.log(e.target);
     if (e.target.id !== 'modal-toggler') {
-      needModal.set(false)
+      needModal.set(false);
     }
   }} />
 
 <svelte:window
   on:scroll={toggleStickyNav}
   on:resize={() => {
-    windowWidth.set(window.innerWidth)
+    windowWidth.set(window.innerWidth);
   }} />
 
 <div>
   <Banner />
   <NavBar {width} {toggleModal} {toggleNavButtons} {navIsSticky} {stickyNav} />
-  <ContactForm {width} />
+  <main>
+    <About />
+    <ContactForm {width} />
+  </main>
   <Footer />
 </div>
