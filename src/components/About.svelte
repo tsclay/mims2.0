c80f34a620075b920479a985611fd32ba1311348
Tim Clay <tsclay9@gmail.com>
2020-09-16 23:32:37 -0400

    added svg assets ; needs work

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -36,6 +36,28 @@
 
 <div class="component" id="about">
   <h1>About</h1>
+  <!-- <svg viewBox="150 90 70 20" height="20mm" width="100%">
+    <g id="layer1" inkscape:groupmode="layer" inkscape:label="Layer 1">
+      <g transform="matrix(0.86152107,0,0,1,26.507841,0)" id="g1248">
+        <path
+          style="fill:none;stroke:#000000;stroke-width:0.117984px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+          d="m 36,91.916667 h 8 v 0"
+          id="path1059" />
+        <path
+          id="path1059-5"
+          d="m 32,96.321565 h 16 v 0"
+          style="fill:none;stroke:#000000;stroke-width:0.166854px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" />
+        <path
+          id="path1059-4"
+          d="m 24,100.72647 h 32 v 0"
+          style="fill:none;stroke:#000000;stroke-width:0.235968px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" />
+        <path
+          id="path1059-9"
+          d="m 8,105.13136 h 64 v 0"
+          style="fill:none;stroke:#000000;stroke-width:0.333709px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" />
+      </g>
+    </g>
+  </svg> -->
   <div class="about-containers pic-left-side">
     <img src="./assets/history.jpg" alt="Mims Painting, 1925" />
     <div class="about-text">
