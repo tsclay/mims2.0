a1fee499b769dc71d8d32f72475e5fb5da8b25e3
Tim <tsclay9@gmail.com>
2020-09-16 20:13:02 -0400

    added some data for Testimonials

--- src/components/Testimonials.svelte
+++ src/components/Testimonials.svelte
@@ -6,10 +6,10 @@
   }
 </style>
 
-<div id="testimonials">
+<div class="component" id="testimonials">
+  <h1>Recognized in the community</h1>
   <p>
     "Thank you for the superb craft you bring to our colors. -Don, Donald
     Kaufman Color"
   </p>
-  <p />
 </div>
