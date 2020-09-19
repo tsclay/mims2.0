403810ccf940433061b264f40ce460885a51cb70
Tim <tsclay9@gmail.com>
2020-09-19 17:10:11 -0400

    fixed position bug ; parent div didn't have pos: relative set

--- src/components/Banner.svelte
+++ src/components/Banner.svelte
@@ -2,13 +2,12 @@
 </script>
 
 <style type="text/scss">
-  #banner > img {
-    width: 100%;
+  #banner {
     position: relative;
-  }
-
-  h3 {
-    font-size: 2rem;
+    img {
+      width: 100%;
+      position: relative;
+    }
   }
 
   .banner-text-wrapper {
@@ -16,14 +15,14 @@
     display: flex;
     flex-flow: column nowrap;
     align-items: center;
-    top: 0;
-    transform: translateY(80%);
+    bottom: 50%;
+    transform: translateY(50%);
     width: 100%;
   }
 
   .logo-wrapper {
     width: 70%;
-    background: rgba(141, 141, 141, 0.664);
+    background: rgba(141, 141, 141, 0.753);
     border: black solid 4px;
     max-width: 920px;
     padding: 8px;
@@ -34,7 +33,7 @@
 </style>
 
 <div id="banner">
-  <img src="../assets/img/rekean2.jpg" alt="Mims Family Painting" />
+  <img src="../assets/img/inside.jpg" alt="Mims Family Painting" />
   <div class="banner-text-wrapper">
     <div class="logo-wrapper">
       <img src="../assets/img/NEWNEWLOGO.png" alt="logo" />
