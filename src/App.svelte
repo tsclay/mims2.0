d2cd2f11d3770b7962a0e3f9125604ab3a990958
Tim Clay <tsclay9@gmail.com>
2020-09-21 17:25:37 -0400

    formatting

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,32 @@
 <script>
-  import NavBar from "./components/NavBar.svelte";
-  import ContactForm from "./components/ContactForm.svelte";
-  import Banner from "./components/Banner.svelte";
-  import About from "./components/About.svelte";
-  import Testimonials from "./components/Testimonials.svelte";
-  import Footer from "./components/Footer.svelte";
-  import { windowWidth, needModal } from "./stores.js";
+  import NavBar from './components/NavBar.svelte'
+  import ContactForm from './components/ContactForm.svelte'
+  import Banner from './components/Banner.svelte'
+  import About from './components/About.svelte'
+  import Testimonials from './components/Testimonials.svelte'
+  import Footer from './components/Footer.svelte'
+  import { windowWidth, needModal } from './stores.js'
 
-  export let toggleModal;
-  export let width;
-  let navIsSticky = false;
+  export let toggleModal
+  export let width
+  let navIsSticky = false
   const stickyNav = `position: sticky;
     position: -webkit-sticky;
     top: 0;
-    left: 0;`;
+    left: 0;`
 
-  const unsubscribeWidth = windowWidth.subscribe((value) => (width = value));
+  const unsubscribeWidth = windowWidth.subscribe((value) => (width = value))
   const unsubscribeModal = needModal.subscribe((value) => {
-    toggleModal = value;
-  });
+    toggleModal = value
+  })
 
   const toggleNavButtons = () => {
-    needModal.set(!toggleModal);
-  };
+    needModal.set(!toggleModal)
+  }
 
   const toggleStickyNav = () => {
-    window.scrollY >= 700 ? (navIsSticky = true) : (navIsSticky = false);
-  };
+    window.scrollY >= 700 ? (navIsSticky = true) : (navIsSticky = false)
+  }
 </script>
 
 <style type="text/scss">
@@ -46,14 +46,14 @@
 <svelte:body
   on:click={(e) => {
     if (e.target.id !== 'modal-toggler') {
-      needModal.set(false);
+      needModal.set(false)
     }
   }} />
 
 <svelte:window
   on:scroll={toggleStickyNav}
   on:resize={() => {
-    windowWidth.set(window.innerWidth);
+    windowWidth.set(window.innerWidth)
   }} />
 
 <div>
