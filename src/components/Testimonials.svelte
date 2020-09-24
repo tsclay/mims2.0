81fc30187a0472707be6da377963a5b3ac5fa407
Tim Clay <tsclay9@gmail.com>
2020-09-24 13:50:03 -0400

    added content and styled quotes into quote blocks

--- src/components/Testimonials.svelte
+++ src/components/Testimonials.svelte
@@ -1,15 +1,73 @@
 <script>
 </script>
 
-<style>
-  #testimonials {
+<style type="text/scss">
+  .quote-block {
+    background: rgba(141, 141, 141, 0.753);
+    margin-bottom: 1rem;
+    border: 4px solid black;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column nowrap;
+    p {
+      margin: 1rem 0;
+      width: 90%;
+    }
+  }
+  .quote-body {
+    line-height: 1.75rem;
+    font-style: italic;
+  }
+  .quote-author {
+    width: 90%;
+    text-align: right;
   }
 </style>
 
 <div class="component" id="testimonials">
   <h1>Recognized in the community</h1>
-  <p>
-    "Thank you for the superb craft you bring to our colors. -Don, Donald
-    Kaufman Color"
-  </p>
+  <div class="quote-block">
+    <p class="quote-body">
+      "Thank you for the superb craft you bring to our colors."
+    </p>
+    <p class="quote-author">Don, Donald Kaufman Color</p>
+  </div>
+  <div class="quote-block">
+    <p class="quote-body">
+      "Thank you for the great job you did at my home! It looks better than new!
+      It has been so nice to work with a company as professional as yours."
+    </p>
+    <p class="quote-author">John</p>
+  </div>
+  <div class="quote-block">
+    <p class="quote-body">
+      "Mims recently painted a house I built in Wainscott. The crew did a superb
+      job. It was thorough and careful. A lot of painting had to be done after
+      we moved in and this was accomplished with great care and cleanliness. I
+      would strongly recommend Mims Painting."
+    </p>
+    <p class="quote-author">Bob</p>
+  </div>
+  <div class="quote-block">
+    <p class="quote-body">
+      "Our house came out beautiful. My wife is very happy."
+    </p>
+    <p class="quote-author">David</p>
+  </div>
+  <div class="quote-block">
+    <p class="quote-body">
+      "I cannot say enough about how courteous and efficient your crew is. They
+      are to be commended and you for having them so well-trained."
+    </p>
+    <p class="quote-author">Naveyz</p>
+  </div>
+  <div class="quote-block">
+    <p class="quote-body">
+      "Thank you for a great job. Our house has <strong>never</strong> looked better.
+      And the guys were a pleasure to have around."
+    </p>
+    <p class="quote-author">Alex</p>
+  </div>
 </div>
