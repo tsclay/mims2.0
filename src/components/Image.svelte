252e04d24c2bdfaab4b962c13da5965e64c06c66
Tim <tsclay9@gmail.com>
2020-10-01 20:38:03 -0400

    components for lazy loading images

---
+++ src/components/Image.svelte
@@ -0,0 +1,53 @@
+<script>
+  import VisibilityGuard from "./VisibilityGuard.svelte";
+  import { fade } from "svelte/transition";
+
+  export let imgSrc = "#";
+  export let imgSrcTiny = imgSrc;
+  export let imgAlt = "Image";
+  export let gridArea;
+
+  let isAbsolute = false;
+  let isStatic = false;
+
+  function setPosToAbsolute() {
+    isAbsolute = true;
+    isStatic = false;
+  }
+  function setPosToStatic() {
+    isAbsolute = false;
+    isStatic = true;
+  }
+</script>
+
+<style>
+  .isAbsolute {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .isStatic {
+    position: static;
+  }
+
+  img {
+    width: 100%;
+    align-self: stretch;
+    height: 100%;
+    min-height: 290px;
+    border-radius: 4px;
+  }
+</style>
+
+<VisibilityGuard let:hasBeenVisible {gridArea}>
+  {#if hasBeenVisible}
+    <img in:fade class:isAbsolute class:isStatic src={imgSrc} alt={imgAlt} />
+  {:else}
+    <img
+      out:fade
+      on:outrostart={setPosToAbsolute}
+      on:outroend={setPosToStatic}
+      src={imgSrcTiny}
+      alt={imgAlt} />
+  {/if}
+</VisibilityGuard>
